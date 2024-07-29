1f2a42a9dfaf7c1388a46b78ff347cdfd515897e
moeumwy <1yjcfff@gmail.com>
2024-07-29 20:45:35 +0800

    Create AuthController.php

---
+++ app/Http/Controllers/V1/Passport/AuthController.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace App\Http\Controllers\V1\Passport;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Passport\AuthForget;
+use App\Http\Requests\Passport\AuthLogin;
+use App\Http\Requests\Passport\AuthRegister;
+use App\Jobs\SendEmailJob;
+use App\Models\InviteCode;
+use App\Models\Plan;
+use App\Models\User;
+use App\Services\AuthService;
+use App\Utils\CacheKey;
+use App\Utils\Dict;
+use App\Utils\Helper;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use ReCaptcha\ReCaptcha;
+use Illuminate\Support\Facades\Http;
+
+class AuthController extends Controller
+{
+    public function loginWithMailLink(Request $request)
+    {
+
+    }
+
+    public function register(AuthRegister $request)
+    {
+        if ((int)config('v2board.register_limit_by_ip_enable', 0)) {
+            $registerCountByIP = Cache::get(CacheKey::get('REGISTER_IP_RATE_LIMIT', $request->ip())) ?? 0;
+            if ((int)$registerCountByIP >= (int)config('v2board.register_limit_count', 3)) {
+                abort(500, __('Register frequently, please try again after :minute minute', [
+                    'minute' => config('v2board.register_limit_expire', 60)
+                ]));
+            }
+        }
+        $userIP = $request->ip();
+        if ((int)config('v2board.recaptcha_enable', 0)) {
+
+            $secret = config('v2board.recaptcha_key');
+            $response = $request->input('recaptcha_data');
+
+            $response = Http::post('https://challenges.cloudflare.com/turnstile/v0/siteverify', [
+                'secret' => $secret,
+                'response' => $response,
+                'ip' => $userIP,
+            ]);
+
+            if ($response->failed()) {
+                abort(500, ('Failed to verify turnstile'));
+            }
+
+            $responseData = $response->json();
+
+            if (!isset($responseData['success']) && !$responseData['success'] === true) {
+                abort(500, ('Invalid code is incorrect'));
+            } 
+        }
+        if ((int)config('v2board.email_whitelist_enable', 0)) {
+            if (!Helper::emailSuffixVerify(
+                $request->input('email'),
+                config('v2board.email_whitelist_suffix', Dict::EMAIL_WHITELIST_SUFFIX_DEFAULT))
+            ) {
+                abort(500, __('Email suffix is not in the Whitelist'));
+            }
+        }
+        if ((int)config('v2board.email_gmail_limit_enable', 0)) {
+            $prefix = explode('@', $request->input('email'))[0];
+            if (strpos($prefix, '.') !== false || strpos($prefix, '+') !== false) {
+                abort(500, __('Gmail alias is not supported'));
+            }
+        }
+        if ((int)config('v2board.stop_register', 0)) {
+            abort(500, __('Registration has closed'));
+        }
+        if ((int)config('v2board.invite_force', 0)) {
+            if (empty($request->input('invite_code'))) {
+                abort(500, __('You must use the invitation code to register'));
+            }
+        }
+        if ((int)config('v2board.email_verify', 0)) {
+            if (empty($request->input('email_code'))) {
+                abort(500, __('Email verification code cannot be empty'));
+            }
+            if ((string)Cache::get(CacheKey::get('EMAIL_VERIFY_CODE', $request->input('email'))) !== (string)$request->input('email_code')) {
+                abort(500, __('Incorrect email verification code'));
+            }
+        }
+        $email = $request->input('email');
+        $password = $request->input('password');
+        $exist = User::where('email', $email)->first();
+        if ($exist) {
+            abort(500, __('Email already exists'));
+        }
+        $user = new User();
+        $user->email = $email;
+        $user->password = password_hash($password, PASSWORD_DEFAULT);
+        $user->uuid = Helper::guid(true);
+        $user->token = Helper::guid();
+        if ($request->input('invite_code')) {
+            $inviteCode = InviteCode::where('code', $request->input('invite_code'))
+                ->where('status', 0)
+                ->first();
+            if (!$inviteCode) {
+                if ((int)config('v2board.invite_force', 0)) {
+                    abort(500, __('Invalid invitation code'));
+                }
+            } else {
+                $user->invite_user_id = $inviteCode->user_id ? $inviteCode->user_id : null;
+                if (!(int)config('v2board.invite_never_expire', 0)) {
+                    $inviteCode->status = 1;
+                    $inviteCode->save();
+                }
+            }
+        }
+
+        // try out
+        if ((int)config('v2board.try_out_plan_id', 0)) {
+            $plan = Plan::find(config('v2board.try_out_plan_id'));
+            if ($plan) {
+                $user->transfer_enable = $plan->transfer_enable * 1073741824;
+                $user->plan_id = $plan->id;
+                $user->group_id = $plan->group_id;
+                $user->expired_at = time() + (config('v2board.try_out_hour', 1) * 3600);
+                $user->speed_limit = $plan->speed_limit;
+            }
+        }
+
+        if (!$user->save()) {
+            abort(500, __('Register failed'));
+        }
+        if ((int)config('v2board.email_verify', 0)) {
+            Cache::forget(CacheKey::get('EMAIL_VERIFY_CODE', $request->input('email')));
+        }
+
+        $user->last_login_at = time();
+        $user->save();
+
+        if ((int)config('v2board.register_limit_by_ip_enable', 0)) {
+            Cache::put(
+                CacheKey::get('REGISTER_IP_RATE_LIMIT', $request->ip()),
+                (int)$registerCountByIP + 1,
+                (int)config('v2board.register_limit_expire', 60) * 60
+            );
+        }
+
+        $authService = new AuthService($user);
+
+        return response()->json([
+            'data' => $authService->generateAuthData($request)
+        ]);
+    }
+
+    public function login(AuthLogin $request)
+    {
+
+    }
+
+    public function token2Login(Request $request)
+    {
+
+        }
+    }
+
+    public function getQuickLoginUrl(Request $request)
+    {
+
+    }
+
+    public function forget(AuthForget $request)
+    {
+
+    }
+}
