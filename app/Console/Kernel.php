ed77eaafeea626bbf5fa0fafb09bca8c0d53a5e5
moeumwy <1yjcfff@gmail.com>
2024-07-30 19:09:58 +0800

    Create Kernel.php

---
+++ app/Console/Kernel.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console;
+
+use App\Utils\CacheKey;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Cache;
+use App\Models\Tokenrequest;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        //
+    ];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+      //省略
+        //tg_notify_rank
+        $schedule->command('notify:rank')->dailyAt('0:05');
+
+      //省略
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__ . '/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
